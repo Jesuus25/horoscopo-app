8532bffc11aa3d976a35d3756a6625a1a1c03378
JesusErick25 <jesuserickquinto25@gmail.com>
2023-09-25 21:05:26 -0500

    primer paso

---
+++ app/src/main/java/com/chunmaru/horoscopoapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,24 @@
+package com.chunmaru.horoscopoapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chunmaru.horoscopoapp.R
+import com.chunmaru.horoscopoapp.databinding.FragmentHoroscopoBinding
+import com.chunmaru.horoscopoapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/chunmaru/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package com.chunmaru.horoscopoapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chunmaru.horoscopoapp.R
+import com.chunmaru.horoscopoapp.databinding.FragmentHoroscopoBinding
+import com.chunmaru.horoscopoapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
